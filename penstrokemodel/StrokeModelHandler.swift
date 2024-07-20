2a6b258163d7a2d0c4b4848eddbf7c7f1f44f279
yujin <northwindapps@gmail.com>
2024-07-20 10:49:12 +0900

    new dataset

--- penstrokemodel/StrokeModelHandler.swift
+++ penstrokemodel/StrokeModelHandler.swift
@@ -5,7 +5,7 @@ class StrokeModelHandler {
     private var interpreter: Interpreter?
     let labels = ["f", "i", "j", "k", "p", "t", "x", "y"]
     let labels_1stroke = ["a", "b", "bksla", "c", "d", "e", "g", "h", "hl", "j", "l", "m", "n", "o", "opb", "q", "r", "s", "sla", "u", "v","vl",  "vl3","w", "z"]
-    let labels19 = ["c", "hlbksla", "j", "s", "v", "vlsla"]
+    let labels19 = ["c", "f", "hlbksla", "j", "s", "t", "v", "vlsla"]
     
 
     init(modelName: String) {
